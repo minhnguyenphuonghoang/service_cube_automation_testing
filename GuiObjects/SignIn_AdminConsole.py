abada6c2b900ae0d402a9094bc7f173caa25663b
Minh Nguyen <minh.filestring@midoplay.com>
2016-10-13 11:53:53 +0700

    add resources

---
+++ GuiObjects/SignIn_AdminConsole.py
@@ -0,0 +1,12 @@
+# Author: Minh.nguyen
+# Date Created: Oct 13, 2016
+#
+
+SignIn_Admin_EmailAddressTextField = '//input[@id="email"]'
+SignIn_Admin_PasswordTextField = '//input[@id="password"]'
+SignIn_Admin_StaySignInCheckbox = '//input[@id="keepSession"]'
+SignIn_Admin_SignInButton = '//button[@id="login"]'
+SignIn_Admin_ForgotPasswordLink = '//a[@href="/Account/ResetPassword"]'
+#SignIn_Admin_
+#SignIn_Admin_
+
