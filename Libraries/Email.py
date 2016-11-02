51fc14cc6453ce25a00f908a2716350427426a9a
Minh Nguyen <minh.filestring@midoplay.com>
2016-11-02 16:04:02 +0700

    pushing new email library

--- Libraries/Email.py
+++ Libraries/Email.py
@@ -1,21 +1,23 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-
-
 import imaplib
 import email
 import re
 from email.header import decode_header
-import time 
+import time
 
 """
 @Author: Minh.nguyen
 @Created Date: Friday August 14, 2015
 """
 
-
 class Email:
-
+    """
+    The flow should be as decribed below:
+    1. Connect to mailbox by keyword _Connect To Email_
+    2. Do whatever you want
+    3. Close Connection by keyword _Close Connection_
+    """
     def __init__(self):
         self.conn = None
 
@@ -29,11 +31,44 @@ class Email:
     def close_connection (self):
         self.conn.close()
         self.conn.logout()
-        
+
+    def wait_for_email(self, subject='any',timeout=120, time_step=10):
+        '''Wait for new mail by
+        - If subject='any', any new email will be accepted
+        - If subject='something', it'll wait until email which has subject is 'something' arrives
+        '''
+        try:
+            timeout = int(timeout)
+        except:
+            print 'timeout must be a number, set default timeout = 120 seconds'
+            timeout = 120
+        expected_time = timeout
+        try:
+            time_step = int(time_step)
+        except:
+            print 'time_step must be a number, set default time_step = 10 seconds'
+            time_step = 30
+        while(timeout >= time_step):            
+            self.conn.recent()
+            result, email_uids = self._get_unseen_emails_by_subject(subject)
+            if result==True:
+                return email_uids
+            time.sleep(time_step)
+            timeout -= time_step
+        if(timeout >= 0):
+            time.sleep(timeout)
+            self.conn.recent()
+            result, email_uids = self._get_unseen_emails_by_subject(subject)
+            if result==True:
+                return email_uids
+        if subject == 'any':
+            assert False, "No email arrives after %d!" % expected_time
+        assert False, "No email with subject: '%s' arrives after %d!" % (subject, expected_time)
+
     def get_all_links(self, email_uid, label='inbox'):
-        #select label
-        self.conn.list()
-        self.conn.select(label)
+        '''Get all links
+        '''
+        self._select_label(label)
 
         result, data = self.conn.uid('search', None, "ALL")
         email_uids = data[0].split()
@@ -59,20 +94,9 @@ class Email:
         urls = re.findall(r'href=[\'"]?([^\'" >]+)', save_string)
         return urls
 
-
-
-    def get_a_link_in_email(self, email_uid, expected_link, label='inbox'):
+    def get_a_link_in_email(self, email_uid, link_index, label='inbox'):
         """
-        Get a link in email by using:
-        - index
-        - link text
-
-        Arguments: 
-        - email_uid: which email do you want to get link
-        - expected_link: index or link_text
-
-        Return:
-        - url
+        Get a link by using index
         """
 
         all_links = self.get_all_links(email_uid, label)
@@ -81,29 +105,29 @@ class Email:
         try:
             expected_link = int(expected_link)
         except ValueError:
-            expected_link = str(expected_link)
+            assert False, 'link index must be a number'
+        try:
+            return all_links[expected_link]
+        except IndexError:
+            assert False, 'Email contains only %d linkzs while you are\
+             expecting link at index: %d' % (len(all_links), expected_link)
 
-        type_of_link = type(expected_link)
+    def get_email_subject(self, index=-1, label='inbox'):
+        self._select_label(label)
 
-        if(type_of_link == int):
-            try:
-                return all_links[expected_link]
-            except IndexError:
-                assert False, 'Email contains only %d links while you are\
-                 expecting link at index: %d' % (len(all_links), expected_link)
+        result, data = self.conn.uid('search', None, "ALL")
+        try:
+            email_uid = data[0].split()[index]
+        except IndexError:
+            return None
+        result, email_data = self.conn.uid('fetch', email_uid, '(RFC822)')
+        raw_email = email_data[0][1]
+        
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
 
-        elif(type_of_link == str or type_of_link == unicode):        
-            print "not implemented"
-            # for item in all_links:
-            #     if expected_link in item: 
-            #         return item
-            # assert False, "Email doesn't contain any link with link text: %s" % expected_link
-
-        else:
-            assert False, 'Unexpected argument type: %s' % type_of_link
-
-# review here
-
+        subject = decode_header(email_message['Subject'])        
+        return subject[0][0]
 
     def get_body_content(self, index=-1, label='inbox'):
         #select label
@@ -129,29 +153,8 @@ class Email:
                 continue      
         return save_string
 
-    def get_email_subject(self, index=-1, label='inbox'):
-        #select label
-        self.conn.list()
-        self.conn.select(label)
-
-        result, data = self.conn.uid('search', None, "ALL")
-        try:
-            email_uid = data[0].split()[index]
-        except IndexError:
-            return None
-        result, email_data = self.conn.uid('fetch', email_uid, '(RFC822)')
-        raw_email = email_data[0][1]
-        
-        raw_email_string = raw_email.decode('utf-8')
-        email_message = email.message_from_string(raw_email_string)
-
-        subject = decode_header(email_message['Subject'])        
-        return subject[0][0]
-    
-    def delete_all_email(self, label='inbox'):
-        #select label
-        self.conn.list()
-        self.conn.select(label)
+    def delete_all_emails(self, label='inbox'):
+        self._select_label(label)
 
         self.conn.recent()
         result, data = self.conn.search(None, "ALL") 
@@ -159,56 +162,18 @@ class Email:
             self.conn.store(email_uid, '+FLAGS', '\\Deleted')
 
     def delete_an_email(self, email_uid, label='inbox'):
-        #select label
-        self.conn.list()
-        self.conn.select(label)
+        self._select_label(label)
 
         self.conn.recent()
         self.conn.uid('STORE', email_uid, '+FLAGS', r'\Deleted')
-            
 
-    def check_email_content(self, expected_content, compare_type=0, index=-1, label='inbox'):
-        res = ''
-        #compare type = 0: ralative comparition (check if content contains expected string)
-        if(compare_type == 0):
-            ## To fix case having 2 or more emails in mailbox ==> cannot get the expected email correctly,
-            ## Mr Nguyen assumes subject is correct and do not need to check.
-            ## I will base on expected subject to find expected email.
-            try:
-                index = self.get_email_index_by_subject(expected_content['subject'])
-            except:
-                assert False, "There is no email which has subject \"%s\"" % expected_content['subject']
-            actual_subject = self.get_email_subject(index, label)            
-            if not expected_content['subject'] in actual_subject: 
-                res += "+ Subject does NOT match!\n"
-                res += "     Actual subject: %s\n" % actual_subject
-                res += "     Expected subject: %s\n" % expected_content['subject']
-            actual_body = self.get_body_content(index, label)
-            
-            for item in expected_content['body']:
-                if not item in actual_body: 
-                    res += "+ Body does NOT contain: %s\n" % item
 
-##            if (res != ''):
-##                raise AssertionError(res)
-            
-
-            return res
-
-        #compare_type = 1: absolute comparition (exactly the same)
-        elif(compare_type == 1):
-            print 'to be implemented'    
-            return 'Function is being implemented, please choose compare_type = 0'
-            
-        else:
-            return 'Invalid value for 2nd argument (compare_type)'
-
-    def check_email_content_new(self, email_subject, *message):
+    def check_email_content(self, email_subject, *message):
         from bs4 import BeautifulSoup 
         import re
 
         try:
-            index = self.get_email_index_by_subject(email_subject)
+            index = self._get_email_index_by_subject(email_subject)
         except:
             assert False, "There is no email which has subject \"%s\"" % email_subject
         
@@ -227,131 +192,52 @@ class Email:
                     result = False
                     errors += "+ Email body does not contain: %s\n" % item
         return result, errors
+    
 
+    
+    
 
+    # private methods
+    
+    def _get_unseen_emails_by_subject(self, subject, label='inbox'):
+        self._select_label(label, True)
+        
+        result, data = self.conn.uid('search', None, "UNSEEN")        
+        
+        email_ids = data[0].split()
+        if subject=='any' and len(email_ids)>0:
+            return True, email_ids
 
-    def get_unseen_emails(self, label = 'inbox'):
-        #select label
+        for email_uid in email_ids:
+            result, email_data = self.conn.uid('fetch', email_uid, '(RFC822)')
+            curr_subject = decode_header(email.message_from_string(email_data[0][1].decode('utf-8'))['Subject'])
+            if curr_subject[0][0] == subject:
+                return True, email_uid
+        return False, None
+            
+
+    def _select_label(self, label='inbox', is_read_only=False):
         self.conn.list()
-        self.conn.select(label)
-
-        result, data = self.conn.uid('search', None, "UNSEEN")
-        return data[0].split()
+        self.conn.select(label, readonly=is_read_only)
 
 
-
-    """
-    @Edited by Minh.nguyen
-    @Description: By default, if user inputs incorrect timeout, time_step, this keyword will assigned 120 to timeout, 30 to time_step
-    """
-    def wait_for_email(self, timeout=120, time_step=30):
-
-        try:
-            timeout = int(timeout)
-        except:
-            timeout = 120
-        
-        expected_time = timeout
-
-        try:
-            time_step = int(time_step)
-        except:
-            time_step = 30
-
-        while(timeout >= time_step):            
-            self.conn.recent()
-            if len(self.get_unseen_emails()) > 0:
-                return -1
-            time.sleep(time_step)
-            timeout -= time_step
-
-        if(timeout >= 0):
-            time.sleep(timeout)
-            self.conn.recent()
-            if len(self.get_unseen_emails()) > 0:
-                return -1
-        assert False, "No email arrives after %d!" % expected_time
-
-
-    def wait_for_email_by_subject(self, subject='any',timeout=120, time_step=30):
-
-        try:
-            timeout = int(timeout)
-        except:
-            timeout = 120
-        
-        expected_time = timeout
-
-        try:
-            time_step = int(time_step)
-        except:
-            time_step = 30
-
-        while(timeout >= time_step):            
-            self.conn.recent()
-            result, email_uids = self._get_unseen_email_by_subject(subject)
-
-            if result==True:
-                return email_uids
-
-            time.sleep(time_step)
-            timeout -= time_step
-
-        if(timeout >= 0):
-            time.sleep(timeout)
-            self.conn.recent()
-            result, email_uids = self._get_unseen_email_by_subject(subject)
-
-            if result==True:
-                return email_uids
-        if subject == 'any':
-            assert False, "No email arrives after %d!" % expected_time
-        assert False, "No email with subject: '%s' arrives after %d!" % (subject, expected_time)
-        
-    def get_email_index_by_subject(self, subject, label = 'inbox'):
-        self.conn.list()
-        self.conn.select(label)
+    def _get_email_index_by_subject(self, subject, label = 'inbox'):
+        self._select_label(label, True)
 
         #Matched email
         result, data = self.conn.uid('search', None, '(HEADER Subject "' + subject + '")')
         ##All email
         result, data2 = self.conn.uid('search', None, "ALL")
-        
-        return data2[0].split().index(data[0].split()[0])
-        
-        
+        return data2[0].split().index(data[0].split()[0])    
     
-    def _get_unseen_email_by_subject(self, subject='any', label = 'inbox'):
-        
-        #self.conn.list()
-        self.conn.select(label, readonly=True)
 
-        result, data = self.conn.uid('search', None, "UNSEEN")
-        
-        email_ids = data[0].split()
-
-        if subject=='any':
-            return True, email_ids
-
-        for email_uid in email_ids:
-
-
-            result, email_data = self.conn.uid('fetch', email_uid, '(RFC822)')
-
-            curr_subject = decode_header(email.message_from_string(email_data[0][1].decode('utf-8'))['Subject'])
-
-            if curr_subject[0][0] == subject:
-                return True, email_uid
-
-        return False, None
-            
-# if __name__ == '__main__':
-#     email_test = Email()
-#     email_test.connect_to_email('automationguru3@gmail.com','AutomationGuru')
-#     uid = email_test.wait_for_email_by_subject("I've shared a file with you",10,3)
-#     print uid
-#     email_test.delete_an_email(uid)
-#     email_test.close_connection()
+if __name__ == '__main__':
+    email_test = Email()
+    email_test.connect_to_email('automationguru3@gmail.com','AutomationGuru')
+    uid = email_test.wait_for_email("any",10,3)
+    print uid
+    # email_test.delete_an_email(uid)
+    email_test.close_connection()
 
 
 
